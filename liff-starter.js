039d70af33459a8ccc50b86a2d85efc9cf707847
yagi asuka <yagi.asuka@donuts.ne.jp>
2020-04-22 18:51:06 +0900

    サンプル追加

--- liff-starter.js
+++ liff-starter.js
@@ -1,1 +1,26 @@
-alert("foo");
+(() => {
+  const app = document.getElementById('app');
+  app.textContent = 'hello,\n';
+
+  liff.init().then((e) => {
+    app.textContent += e;
+    if (liff.isLoggedIn()) {
+      liff.login();
+    } else {
+      return;
+    }
+
+    app.textContent += '\nlanguage:';
+    app.textContent += liff.getLanguage();
+    app.textContent += '\nversion:';
+    app.textContent += liff.getVersion();
+    app.textContent += '\nclient:';
+    app.textContent += liff.isInClient();
+    app.textContent += '\nloggedin:';
+    app.textContent += liff.isLoggedIn();
+    app.textContent += '\nos:';
+    app.textContent += liff.getOS();
+  }).catch((e) => {
+    app.textContent += e;
+  });
+})();
